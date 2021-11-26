1aa8eb7fab17268c629b22c0fb57b47c497169e4
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-11-26 17:43:11 -0300

    AR success view done

--- b.hind/View/AR/CertificateOfAchievementView.swift
+++ b.hind/View/AR/CertificateOfAchievementView.swift
@@ -13,20 +13,100 @@ struct CertificateOfAchievementView : View {
             ZStack {
                 Color.black.edgesIgnoringSafeArea(.all)
                 
+                Image("ar_success_bg")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                
                 GeometryReader { geo in
                     
-                    Image("ar_success_bg")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: geo.size.width, height: geo.size.height)
-                    
                     VStack {
+                        Spacer()
+                        ZStack {
+                            VStack {
+                                Text("Certificate \nof Achievement")
+                                    .font(.custom("JosefinSans-Bold", size: 25))
+                                    .foregroundColor(.black)
+                                    .multilineTextAlignment(.center)
+                                    .padding()
+                                    .frame(width: geo.size.width, height: geo.size.height * 0.1, alignment: .center)
+                                    .padding(.top, 20)
+                                
+                                Text("We congratulate _______________ on succesfully capturing the creature _______________ without dying or suffering any irreparable harm!")
+                                    .font(.custom("JosefinSans-Regular", size: 12))
+                                    .lineLimit(5)
+                                    .lineSpacing(10)
+                                    .foregroundColor(.black)
+                                    .multilineTextAlignment(.center)
+                                    .padding()
+                                    .frame(width: geo.size.width * 0.85, alignment: .top)
+                                
+                                Image("b.witched_logo")
+                                
+                                Image("ar_certificate_stamp")
+                                    .frame(width: geo.size.width * 0.88, alignment: .trailing)
+                                    .padding(.top, -20)
+                                
+                            }
+                            .background(
+                                Image("ar_certificate")
+                            )
+                            
+                            Text("(employee name here)")
+                                .font(.custom("SpecialElite-Regular", size: 10))
+                                .foregroundColor(.black)
+                                .padding(.top, -72)
+                                .padding(.leading, 17)
+                            
+                            Text("shade")
+                                .font(.custom("SpecialElite-Regular", size: 10))
+                                .foregroundColor(.black)
+                                .padding(.top, -48)
+                                .padding(.leading, 80)
+                            
+                            Text("©")
+                                .font(.custom("JosefinSans-Regular", size: 20))
+                                .foregroundColor(.black.opacity(0.4))
+                                .padding(.leading, 40)
+                                .frame(width: geo.size.width, height: geo.size.height * 0.46, alignment: .bottomLeading)
+                            
+                            Text("1430")
+                                .font(.custom("JosefinSans-Regular", size: 11))
+                                .foregroundColor(.black.opacity(0.4))
+                                .padding(.leading, 60)
+                                .frame(width: geo.size.width, height: geo.size.height * 0.45, alignment: .bottomLeading)
+                            
+                            Text("B.WITCHED INC.")
+                                .font(.custom("JosefinSans-Regular", size: 11))
+                                .foregroundColor(.black.opacity(0.4))
+                                .frame(width: geo.size.width, height: geo.size.height * 0.45, alignment: .bottom)
+                            
+                            Text("MADE IN BR")
+                                .font(.custom("JosefinSans-Regular", size: 11))
+                                .foregroundColor(.black.opacity(0.4))
+                                .padding(.trailing, 50)
+                                .frame(width: geo.size.width, height: geo.size.height * 0.45, alignment: .bottomTrailing)
+                        }
                         
+                        NavigationLink(destination: HomeView()) {
+                            Text("Back to Home")
+                                .font(.custom("JosefinSans-SemiBold", size: 17))
+                                .foregroundColor(.black)
+                                .frame(width: geo.size.width * 0.6, height: geo.size.height * 0.07, alignment: .center)
+                                .background(Color("certificate_back_button_bg"))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
+                        }
+                        .frame(width: geo.size.width, height: geo.size.height * 0.2, alignment: .center)
+                        
+                        Spacer()
                     }
+                    .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
                 }
-                .edgesIgnoringSafeArea(.all)
             }
         }
+        .edgesIgnoringSafeArea(.all)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
