8e0b89c4eae9c6cd80a1fbca95eea5738d0a3c0f
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-11-27 17:22:16 -0300

    Home screen done

--- b.hind/View/Home/HomeView.swift
+++ b.hind/View/Home/HomeView.swift
@@ -5,155 +5,197 @@
 import SwiftUI
 
 struct HomeView : View {
+    let size = UIScreen.main.bounds.size
+    
     var body: some View {
         return NavigationView {
             ZStack {
                 Color.black.edgesIgnoringSafeArea(.all)
                 
-                GeometryReader { geo in
-                    Image("home_bg")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: geo.size.width, height: geo.size.height)
+                Image("home_bg")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size.width, height: size.height)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack (alignment: .center) {
+                    buildSettings
+                    buildHint
                     
-                        VStack (alignment: .center) {
-                            VStack (alignment: .leading) {
-                                Text("B.hint")
-                                    .fontWeight(.bold)
-                                    .foregroundColor(Color("home_title"))
-                                    .padding(.bottom, 2)
-                                    .padding(.top, 10)
-                                
-                                Text("Use earphones for a better experience with our spatial audio, we are not responsible for occasional heart attacks or the voices in your head.")
-                                    .font(.system(size: 15))
-                                    .foregroundColor(.white)
-                                    .padding(.bottom, 2)
-                            }
-                            .padding(.horizontal, 25)
-                            .frame(width: geo.size.width * 0.9, height: geo.size.height * 0.16, alignment: .center)
-                            .background(Color("home_hint_bg").opacity(0.72))
-                            .clipShape(RoundedRectangle(cornerRadius: 25))
-                            .background(
-                                RoundedRectangle(cornerRadius: 25)
-                                    .stroke(Color("home_hint_bg").opacity(0.72), lineWidth: 1)
-                            )
-                            .padding(.top, 80)
+                    HStack (alignment: .top) {
+                        Spacer()
+                        VStack (alignment: .center)  {
+                            buildBgoodApp
+                            buildBBankApp
+                        }
+                        .frame(width: size.width * 0.3, alignment: .trailing)
                         
-                            HStack (alignment: .top) {
+                        VStack (alignment: .center) {
+                            HStack {
+                                buildBOnTimeApp
+                                
                                 Spacer()
-                                VStack (alignment: .center)  {
-                                    VStack (alignment: .center) {
-                                        Image("achievements_icon")
-                                                .resizable()
-                                                .scaledToFill()
-                                                .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
-                                        
-                                        Text("B.good")
-                                            .font(.system(size:15))
-                                            .foregroundColor(.white)
-                                    }
-
-                                    VStack (alignment: .center) {
-                                        Image("bank_icon")
-                                                .resizable()
-                                                .scaledToFill()
-                                                .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
-                                            .padding(.top, 10)
-                                        
-                                        Text("B.bank")
-                                            .font(.system(size:15))
-                                            .foregroundColor(.white)
-                                    }
-                                }
-                                Spacer()
-                                VStack (alignment: .center) {
-                                    HStack {
-                                        VStack (alignment: .center) {
-                                            Image("time_icon")
-                                                    .resizable()
-                                                    .scaledToFill()
-                                                    .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
-                                            
-                                            Text("B.on time")
-                                                .font(.system(size:15))
-                                                .foregroundColor(.white)
-                                        }
-                                        Spacer()
-                                        VStack (alignment: .center) {
-                                            Image("grim_icon")
-                                                    .resizable()
-                                                    .scaledToFill()
-                                                .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
-                                            
-                                            Text("B.grim")
-                                                .font(.system(size:15))
-                                                .foregroundColor(.white)
-                                        }
-                                    }
-                                    Image("widget_1")
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: geo.size.width * 0.3, height: geo.size.height * 0.3)
-                                        .padding(.top, 10)
-                                }
-                                .padding(.horizontal, 70)
+                                
+                                buildBGrimApp
                                 
                             }
-                            .padding(.top, 30)
-                            .frame(width: geo.size.width, alignment: .center)
-                            Spacer()
+                            buildWidgetApp
+                            
                         }
-                    .padding(.bottom, 30)
-                    .frame(width: geo.size.width, height: geo.size.height, alignment: .top)
-                    
-                       
-                    
-                    ZStack {
-                        HStack {
-                            NavigationLink(destination: MailView()) {
-                                Image("message_icon")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .trailing)
-                                    .padding(.top, 10)
-                                    .padding(.bottom, 5)
-                            }
-                            NavigationLink(destination: MailView()) {
-                                Image("phone_icon")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
-                                    .padding(.top, 10)
-                                    .padding(.leading, 45)
-                                    .padding(.trailing, 45)
-                                    .padding(.bottom, 5)
-                            }
-                            NavigationLink(destination: MailView()) {
-                                Image("mail_icon")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .leading)
-                                    .padding(.top, 10)
-                                    .padding(.bottom, 5)
-                            }
-                        }
-                        .frame(width: geo.size.width * 0.9, height: geo.size.height * 0.12, alignment: .center)
-                        .background(Color("home_bottom_group").opacity(0.6))
-                        .clipShape(RoundedRectangle(cornerRadius: 32))
-                        .background(
-                            RoundedRectangle(cornerRadius: 32)
-                                .stroke(Color("home_bottom_group").opacity(0.6), lineWidth: 1)
-                        )
+                        .frame(width: size.width * 0.5, alignment: .leading)
+                        .padding(.horizontal, 70)
+                        
+                        Spacer()
                         
                     }
-                    .padding(.bottom, 30)
-                    .frame(width: geo.size.width, height: geo.size.height, alignment: .bottom)
+                    .padding(.top, 20)
+                    .frame(width: size.width, alignment: .center)
+                    Spacer()
                     
+                    buildBottomApps
                 }
+                .padding(.bottom, 30)
+                .frame(width: size.width, height: size.height, alignment: .top)
                 .edgesIgnoringSafeArea(.all)
             }
         }
         .navigationBarBackButtonHidden(true)
+    }
+    
+    var buildSettings: some View {
+        Image("settings_icon")
+            .frame(width: size.width, alignment: .trailing)
+            .padding(.top, 50)
+            .padding(.bottom, 20)
+            .padding(.trailing, 40)
+    }
+    
+    var buildHint: some View {
+        VStack (alignment: .leading) {
+            HStack {
+                Text("B.hint")
+                    .font(.system(size: 17, weight: .semibold))
+                    .foregroundColor(Color("home_title"))
+                    .padding(.bottom, 2)
+                .padding(.top, 10)
+                
+                Spacer()
+                
+                Image("hint_logo")
+            }
+            
+            Text("Use earphones for a better experience with our spatial audio, we are not responsible for occasional heart attacks or the voices in your head.")
+                .font(.system(size: 15, weight: .regular))
+                .foregroundColor(.white)
+                .padding(.bottom, 2)
+        }
+        .padding(.horizontal, 25)
+        .frame(width: size.width * 0.9, height: size.height * 0.16, alignment: .center)
+        .background(Color("home_hint_bg").opacity(0.72))
+        .clipShape(RoundedRectangle(cornerRadius: 25))
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(Color("home_hint_bg").opacity(0.72), lineWidth: 1)
+        )
+    }
+    
+    var buildBgoodApp: some View {
+        VStack (alignment: .center) {
+            Image("achievements_icon")
+                .resizable()
+                .scaledToFill()
+                .frame(width: size.width * 0.1, height: size.height * 0.1, alignment: .center)
+            
+            Text("B.Good")
+                .font(.system(size:12))
+                .foregroundColor(.white)
+        }
+    }
+    
+    var buildBOnTimeApp: some View {
+        VStack (alignment: .center) {
+            Image("time_icon")
+                .resizable()
+                .scaledToFill()
+                .frame(width: size.width * 0.1, height: size.height * 0.1, alignment: .center)
+            
+            Text("B.On Time")
+                .font(.system(size:12))
+                .foregroundColor(.white)
+        }
+    }
+    
+    var buildBGrimApp: some View {
+        VStack (alignment: .center) {
+            Image("grim_icon")
+                .resizable()
+                .scaledToFill()
+                .frame(width: size.width * 0.1, height: size.height * 0.1, alignment: .center)
+            
+            Text("B.Grim")
+                .font(.system(size:12))
+                .foregroundColor(.white)
+        }
+    }
+    
+    var buildBBankApp: some View {
+        VStack (alignment: .center) {
+            Image("bank_icon")
+                .resizable()
+                .scaledToFill()
+                .frame(width: size.width * 0.1, height: size.height * 0.1, alignment: .center)
+                .padding(.top, 10)
+            
+            Text("B.Bank")
+                .font(.system(size:12))
+                .foregroundColor(.white)
+        }
+    }
+    
+    var buildWidgetApp: some View {
+        Image("widget_1")
+            .resizable()
+            .scaledToFill()
+            .frame(width: size.width * 0.3, height: size.height * 0.3)
+            .padding(.top, 10)
+    }
+    
+    var buildBottomApps: some View {
+        HStack {
+            NavigationLink(destination: MailView()) {
+                Image("message_icon")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size.width * 0.1, height: size.height * 0.1, alignment: .trailing)
+                    .padding(.top, 10)
+                    .padding(.bottom, 5)
+            }
+            NavigationLink(destination: MailView()) {
+                Image("phone_icon")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size.width * 0.1, height: size.height * 0.1, alignment: .center)
+                    .padding(.top, 10)
+                    .padding(.leading, 45)
+                    .padding(.trailing, 45)
+                    .padding(.bottom, 5)
+            }
+            NavigationLink(destination: MailView()) {
+                Image("mail_icon")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size.width * 0.1, height: size.height * 0.1, alignment: .leading)
+                    .padding(.top, 10)
+                    .padding(.bottom, 5)
+            }
+        }
+        .frame(width: size.width * 0.9, height: size.height * 0.12, alignment: .center)
+        .background(Color("home_bottom_group").opacity(0.6))
+        .clipShape(RoundedRectangle(cornerRadius: 32))
+        .background(
+            RoundedRectangle(cornerRadius: 32)
+                .stroke(Color("home_bottom_group").opacity(0.6), lineWidth: 1)
+        )
     }
 }
 
