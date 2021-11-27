62d9de1cee26e3bfaa4b3660feed4d0e95099b01
luisa <luisabtrabulci@gmail.com>
2021-11-27 14:36:40 -0300

    Homezinha

--- b.hind/View/Home/HomeView.swift
+++ b.hind/View/Home/HomeView.swift
@@ -16,41 +16,125 @@ struct HomeView : View {
                         .scaledToFill()
                         .frame(width: geo.size.width, height: geo.size.height)
                     
-                    ZStack (alignment: .leading) {
-                        VStack (alignment: .leading) {
+                        VStack (alignment: .center) {
                             VStack (alignment: .leading) {
                                 Text("B.hint")
+                                    .fontWeight(.bold)
                                     .foregroundColor(Color("home_title"))
                                     .padding(.bottom, 2)
+                                    .padding(.top, 10)
                                 
                                 Text("Use earphones for a better experience with our spatial audio, we are not responsible for occasional heart attacks or the voices in your head.")
                                     .font(.system(size: 15))
                                     .foregroundColor(.white)
+                                    .padding(.bottom, 2)
                             }
-                            .padding(.horizontal, 20)
-                        }
-                        .frame(width: geo.size.width * 0.9, height: geo.size.height * 0.16, alignment: .center)
-                        .background(Color("home_hint_bg").opacity(0.72))
-                        .clipShape(RoundedRectangle(cornerRadius: 32))
-                        .background(
-                            RoundedRectangle(cornerRadius: 32)
-                                .stroke(Color("home_hint_bg").opacity(0.72), lineWidth: 1)
-                        )
-                        .padding(.horizontal, 60)
-                        .padding(.top, 80)
+                            .padding(.horizontal, 25)
+                            .frame(width: geo.size.width * 0.9, height: geo.size.height * 0.16, alignment: .center)
+                            .background(Color("home_hint_bg").opacity(0.72))
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .background(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color("home_hint_bg").opacity(0.72), lineWidth: 1)
+                            )
+                            .padding(.top, 80)
                         
-                    }
+                            HStack (alignment: .top) {
+                                Spacer()
+                                VStack (alignment: .center)  {
+                                    VStack (alignment: .center) {
+                                        Image("achievements_icon")
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
+                                        
+                                        Text("B.good")
+                                            .font(.system(size:15))
+                                            .foregroundColor(.white)
+                                    }
+
+                                    VStack (alignment: .center) {
+                                        Image("bank_icon")
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
+                                            .padding(.top, 10)
+                                        
+                                        Text("B.bank")
+                                            .font(.system(size:15))
+                                            .foregroundColor(.white)
+                                    }
+                                }
+                                Spacer()
+                                VStack (alignment: .center) {
+                                    HStack {
+                                        VStack (alignment: .center) {
+                                            Image("time_icon")
+                                                    .resizable()
+                                                    .scaledToFill()
+                                                    .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
+                                            
+                                            Text("B.on time")
+                                                .font(.system(size:15))
+                                                .foregroundColor(.white)
+                                        }
+                                        Spacer()
+                                        VStack (alignment: .center) {
+                                            Image("grim_icon")
+                                                    .resizable()
+                                                    .scaledToFill()
+                                                .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
+                                            
+                                            Text("B.grim")
+                                                .font(.system(size:15))
+                                                .foregroundColor(.white)
+                                        }
+                                    }
+                                    Image("widget_1")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: geo.size.width * 0.3, height: geo.size.height * 0.3)
+                                        .padding(.top, 10)
+                                }
+                                .padding(.horizontal, 70)
+                                
+                            }
+                            .padding(.top, 30)
+                            .frame(width: geo.size.width, alignment: .center)
+                            Spacer()
+                        }
                     .padding(.bottom, 30)
                     .frame(width: geo.size.width, height: geo.size.height, alignment: .top)
+                    
+                       
                     
                     ZStack {
                         HStack {
                             NavigationLink(destination: MailView()) {
-                                Image("mail_icon")
+                                Image("message_icon")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .trailing)
+                                    .padding(.top, 10)
+                                    .padding(.bottom, 5)
+                            }
+                            NavigationLink(destination: MailView()) {
+                                Image("phone_icon")
                                     .resizable()
                                     .scaledToFill()
                                     .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .center)
                                     .padding(.top, 10)
+                                    .padding(.leading, 45)
+                                    .padding(.trailing, 45)
+                                    .padding(.bottom, 5)
+                            }
+                            NavigationLink(destination: MailView()) {
+                                Image("mail_icon")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: geo.size.width * 0.1, height: geo.size.height * 0.1, alignment: .leading)
+                                    .padding(.top, 10)
+                                    .padding(.bottom, 5)
                             }
                         }
                         .frame(width: geo.size.width * 0.9, height: geo.size.height * 0.12, alignment: .center)
