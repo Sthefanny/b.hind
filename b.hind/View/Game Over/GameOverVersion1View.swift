1e1b516499e65ce0d00cd5be2a57fea668302658
Hanna Coutinho <hanna.pcf@gmail.com>
2021-11-29 10:00:35 -0300

    game over button (go back in time = home)

--- b.hind/View/Game Over/GameOverVersion1View.swift
+++ b.hind/View/Game Over/GameOverVersion1View.swift
@@ -26,7 +26,7 @@ struct GameOverVersion1View: View {
                     .frame(width: UIScreen.main.bounds.size.width * 1, height: UIScreen.main.bounds.size.height * 0.003, alignment: .center)
                 
                 Text("The least periodic newspaper you'll ever see")
-                    .font(.custom("JosefinSans-Regular", size: 18))
+                    .font(.custom("JosefinSans-Regular", size: 17))
                     .foregroundColor(.black)
                     .lineLimit(1)
                     .padding(.horizontal, 10)
@@ -42,7 +42,7 @@ struct GameOverVersion1View: View {
                 
                 HStack {
                     VStack (alignment: .leading, spacing: 20) {
-                        Text("6 months before today our headline was about the same subject and we were clueless of so many things. Humanity's last hope was selfish enough to abandon us, and there was nothing we could do.\n\nToday we still have so many questions, but one thing we know for sure: the day of our ruin has arrived!\n\nSupernatural\nbeings have managed to enter our reality, no one can stop them anymore! Without B.Witched’s tools we’re doomed.")
+                        Text("6 months before today our headline was about the same subject and we were clueless of so many things. Humanity's last hope was selfish enough to abandon us, and there was nothing we could do.\n\nToday we still have so many questions, but one thing we know for sure: the day of our ruin has arrived!\n\nSupernatural beings have managed to enter our reality, no one can stop them anymore! Without B.Witched’s tools we’re doomed.")
                             .font(.custom("JosefinSans-Regular", size: 18))
                             .multilineTextAlignment(.leading)
                             .foregroundColor(.black)
@@ -55,7 +55,7 @@ struct GameOverVersion1View: View {
                             .padding(.trailing, 20)
                             .padding(.top, 0)
                         Text("HUMANITY IS OVER, AND IT'S YOUR FAULT.")
-                            .font(.custom("JosefinSans-Bold", size: 18))
+                            .font(.custom("JosefinSans-Bold", size: 17))
                             .multilineTextAlignment(.leading)
                             .foregroundColor(.black)
                             .padding(.trailing, 20)
@@ -66,16 +66,25 @@ struct GameOverVersion1View: View {
                             .foregroundColor(.black)
                             .padding(.trailing, 20)
                             .padding(.top, -16)
-                        Text("Pope John XXIII made a\nstatement at the Vatican “No one can save us anymore, if only we could go back in time.")
-                            .font(.custom("JosefinSans-Regular", size: 18))
+                        Text("Pope John XXIII made a statement at the Vatican “No one can save us anymore, if only we could go back in time.")
+                            .font(.custom("JosefinSans-Regular", size: 17))
                             .multilineTextAlignment(.leading)
                             .foregroundColor(.black)
                             .padding(.trailing, 20)
                         Spacer()
                     }
                 }
-                Text("Espaço para botão")
-                    .foregroundColor(Color.black)
+                NavigationLink(destination: HomeView()) {
+                    Text("Go back in time")
+                        .font(.custom("JosefinSans-Bold", size: 17))
+                        .foregroundColor(.white)
+                        .frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.size.height * 0.07, alignment: .center)
+                        .background(Color("fail_back_button_bg"))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
+                }
+                .padding(.bottom, 10)
+ 
                 Spacer()
             }
             .frame(width: UIScreen.main.bounds.size.width * 1, height: UIScreen.main.bounds.size.height * 1, alignment: .center)
