8666be32faf6a5e6bd34edc32b2cca0862f1951c
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-12-08 14:16:23 -0300

    Adding firebase analytics

--- b.hind/bhindApp.swift
+++ b.hind/bhindApp.swift
@@ -6,6 +6,22 @@
 //
 
 import SwiftUI
+import UIKit
+import Firebase
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct bhindApp: App {
